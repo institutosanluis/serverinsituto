bf51ba4696272a541c02ddac7b4ee7dc4cbf3312
Pliss-App <pliissapp5@gmail.com>
2024-01-13 11:08:16 -0600

    Primer cambio

---
+++ routes/niveles.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { db } = require('../firebase');
+const multer = require('multer');
+const { uploadImage, administrador } = require('../firebase');
+
+const mul = multer({
+    storage: multer.memoryStorage(),
+    limits: 1024 * 1024
+})
+
+
+const nivelesRouter = express.Router();
+
+
+informacionRouter.get('/all', async (req, res) => {
+    const ref = db.database().ref(`Niveles/`);
+    ref.once('value', snapshot => {
+        res.send(snapshot.val());
+    });
+})
+
+module.exports = nivelesRouter;
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const informacionRouter = require('../routes/informacion');
+const nivelesRouter = require('../routes/niveles');
+
 
 const apiRouter = express.Router();
 
 
 apiRouter.use('/informacion', informacionRouter);
+apiRouter.use('/niveles', nivelesRouter);
 
 module.exports = apiRouter;
